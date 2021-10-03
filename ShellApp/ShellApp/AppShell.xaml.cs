9eb89304e3ffc14863cd4134262aaef77dbb73e2
David Dunson <david.dunson@gsdlogic.com>
2021-10-03 07:20:01 -0500

    Add project files.

---
+++ ShellApp/ShellApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ShellApp.ViewModels;
+using ShellApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ShellApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ShellApp/ShellApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ShellApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ShellApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
